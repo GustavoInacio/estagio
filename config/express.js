53e6c1cbf9193fb6de66fa764dee00eed89f84a5
Vinicius <vinicius@nanomedia.com.br>
2015-11-09 00:57:05 -0200

    Adicionado rotas e controllers a aplicação

--- config/express.js
+++ config/express.js
@@ -1,15 +1,22 @@
 
-// Módulo de configuração do Express
+// módulo de configuração do Express na aplicação
 var express = require('express');
+var home = require('../app/routes/home');
 
 module.exports = function() {
  	var app = express();
  	
- 	// Seta a variável de ambiente para um nome de chave-valor
+ 	// variáveis de ambiente
  	app.set('port', 3000);
 
  	// middleware
  	app.use(express.static('./public/'));
+ 	
+ 	// template engine
+ 	app.set('view engine', 'ejs');
+ 	app.set('views', './app/views');
+
+ 	home(app);	
 
  	return app;
 };
